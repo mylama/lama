a5545a3edc1dc639236d34f352217b4f39c3265d
mylama <lama.helpme@gmail.com>
2020-01-05 14:10:45 +0100

    variablen

---
+++ assets/js/version.js
@@ -0,0 +1,6 @@
+    "use strict";
+    let version = "1.8.5";
+    let datum = "Dezember 2019";
+    
+    document.getElementById("vnummer").innerHTML = version;
+    document.getElementById("vdatum").innerHTML = datum;
