329eca3e78aa6e7d8e8e4777b027b7736b67184c
mylama <lama.helpme@gmail.com>
2020-06-18 09:58:18 +0200

    neue version

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "1.8.6";
-    let datum = "Februar 2020";
+    let version = "2.0.0";
+    let datum = "Juni 2020";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
