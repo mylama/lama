b799d34b19b11cc56b85fb628526db956eb81d98
chrisiweb <c.weberndorfer@gmail.com>
2022-03-15 19:16:44 +0100

    update version

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "3.3.0";
-    let datum = "Jänner 2022";
+    let version = "3.4.0";
+    let datum = "März 2022";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
