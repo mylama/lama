507e3545d2791ec0b22d1f6be0adea6b7491b9ea
chrisiweb <c.weberndorfer@gmail.com>
2022-10-06 13:54:20 +0200

    update v4.1.4

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "4.1.3";
-    let datum = "September 2022";
+    let version = "4.1.4";
+    let datum = "Oktober 2022";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
