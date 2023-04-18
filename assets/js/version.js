58b89cdf11522db115d7dec8398c396e1764d692
chrisiweb <c.weberndorfer@gmail.com>
2023-04-18 14:35:23 +0200

    push v4.3.0

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "4.2.1";
-    let datum = "Jänner 2023";
+    let version = "4.3.0";
+    let datum = "April 2023";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
