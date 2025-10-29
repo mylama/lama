2da66d5ce438464ad3cf96d384c5eb6051498cf5
mylama <50013332+mylama@users.noreply.github.com>
2025-10-29 09:42:35 +0100

    versions-update

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "4.6.6";
-    let datum = "April 2025";
+    let version = "5.0.0";
+    let datum = "September 2025";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
