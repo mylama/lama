ac67e59a1bcf71c4d365ece51b55cc83d99d068f
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2026-03-13 08:52:08 +0100

    update v5.0.9

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
     "use strict";
-    let version = "5.0.6";
-    let datum = "Februar 2026";
+    let version = "5.0.9";
+    let datum = "März 2026";
     
     document.getElementById("vnummer").innerHTML = version;
     document.getElementById("vdatum").innerHTML = datum;
